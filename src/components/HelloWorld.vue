a8846ac5a2792e218a684ffd09e0221b2929cddc
loinnn <loinnn1997@gmail.com>
2023-04-19 12:35:33 +0500

    upload project git

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world! Bizning Company
+  </h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+// css
+import './assets/css/styles.css'
+
+const app = createApp(App)
+app.mount('#app')
